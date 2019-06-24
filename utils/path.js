b8688817acaa2e4e6b950d5485b6a4f4db972698
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-24 10:58:35 +0100

    Set up paths

---
+++ utils/path.js
@@ -0,0 +1,10 @@
+const path = {
+  users: '/api/users',
+  auth: '/api/auth',
+  events: '/api/events',
+  tasks: '/api/tasks',
+  shopping: '/api/shopping',
+  vendors: '/api/vendors',
+};
+
+module.exports = path;
