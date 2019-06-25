ae8749eacb08dc78c659870d0feb9d4d95bb5669
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-26 00:45:58 +0100

    Create endpoint for getting a shopping list for an event

--- api/helpers/shoppingListHelpers.js
+++ api/helpers/shoppingListHelpers.js
@@ -20,3 +20,145 @@ const deleteItem = id => db('shopping_list')
 const updateItem = (item, id) => db('shopping_list')
     .where({ id })
     .update(item);
+
+// const markAsCompleted = (task, id) => db('tasks')
+//     .where({ id })
+//     .update(task);
+//
+// const markAsPending = (task, id) => db('tasks')
+//     .where({ id })
+//
+//     .update(task)
+//     .then(ids => getItemById(ids[0]));
+
+
+const notUndefined = (value) => value !== undefined;
+
+const verifyAndCleanItem = (itemName, itemAcquired, eventId, itemPrice) => {
+  if (!itemName && !notUndefined(itemAcquired) && !eventId && !itemPrice) {
+    throw new Error('Invalid item');
+  }
+
+  if (!notUndefined(itemAcquired) && !itemName && !itemPrice) {
+    return {
+      event_id: eventId,
+    };
+  }
+
+  if (!notUndefined(itemAcquired) && !eventId && !itemPrice) {
+    return {
+      item_name: itemName,
+    };
+  }
+
+  if (!notUndefined(itemAcquired) && !eventId && !eventId) {
+    return {
+      item_price: itemPrice,
+    };
+  }
+
+  if (!itemName && !eventId && !itemPrice) {
+    return {
+      item_acquired: itemAcquired,
+    };
+  }
+
+  if (!itemName && !eventId && !notUndefined(itemAcquired)) {
+    return {
+      item_price: itemPrice,
+    };
+  }
+
+  if (!itemName && !eventId) {
+    return {
+      item_price: itemPrice,
+      item_acquired: itemAcquired,
+    }
+  }
+
+  if (!itemName && !itemPrice) {
+    return {
+      event_id: eventId,
+      item_acquired: itemAcquired,
+    }
+  }
+
+  if (!itemName && !notUndefined(itemAcquired)) {
+    return {
+      item_price: itemPrice,
+      event_id: eventId,
+    }
+  }
+
+  if (!notUndefined(itemAcquired) && !eventId) {
+    return {
+      item_price: itemPrice,
+      item_name: itemName,
+    }
+  }
+
+  if (!notUndefined(itemAcquired) && !itemPrice) {
+    return {
+      event_id: eventId,
+      item_name: itemName,
+    }
+  }
+
+  if (!itemPrice && !eventId) {
+    return {
+      item_acquired: itemAcquired,
+      item_name: itemName,
+    }
+  }
+
+  if (!itemName) {
+    return {
+      item_acquired: itemAcquired,
+      event_id: eventId,
+      item_price: itemPrice,
+    };
+  }
+
+  if (!eventId) {
+    return {
+      item_acquired: itemAcquired,
+      item_name: itemName,
+      item_price: itemPrice,
+    };
+  }
+
+  if (!notUndefined(itemAcquired)) {
+    return {
+      item_name: itemName,
+      event_id: eventId,
+      item_price: itemPrice,
+    };
+  }
+
+  if (!itemPrice) {
+    return {
+      item_name: itemName,
+      event_id: eventId,
+      item_acquired: itemAcquired,
+    };
+  }
+
+  return {
+    item_name: itemName,
+    item_acquired: itemAcquired,
+    event_id: eventId,
+  };
+};
+
+module.exports = {
+  getShoppingList,
+  getItemById,
+  getShoppingListForEvent,
+  addItem,
+  deleteItem,
+  updateItem,
+  notUndefined,
+  verifyAndCleanItem,
+
+};
+
---
+++ routes/shoppingListRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { restricted } = require('../middleware/restricted');
+
+const router = express.Router();
+const db = require('../data/dbConfig');
+const errorMessage = require('../utils/errorMessage');
+const responseMessage = require('../utils/responseMessage');
+
+
+const ShoppingList = require('../api/helpers/shoppingListHelpers');
+const Events = require('../api/helpers/eventsHelpers');
+
+// [GET] shopping list for event
+router.get('/', (req, res) => {
+  if (req.query && req.query.event_id) {
+    ShoppingList.getShoppingListForEvent(req.query.event_id)
+        .then((items) => {
+          if (!items) {
+            res.status(404).json(errorMessage.shoppingListNotFound);
+          } else {
+            res.status(200).json(items);
+          }
+        })
+        .catch((error) => {
+          res.status(500).json(errorMessage.shoppingListNotRetrieved);
+        });
+  } else {
+    ShoppingList.getShoppingList()
+        .then((items) => {
+          res.status(200).json(items);
+        })
+        .catch((error) => {
+          res.status(500).json(errorMessage.shoppingListNotRetrieved);
+        });
+  }
+});
