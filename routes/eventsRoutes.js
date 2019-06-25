8f2f397eeeed5b856582992923b48c552e796606
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-25 12:42:15 +0100

    Create endpoint for getting a single event with tasks

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -35,3 +35,50 @@ router.get('/', (req, res) => {
         });
   }
 });
+
+// [GET] event by id
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params;
+//   Events.getEventById(id)
+//       .then((event) => {
+//         if (!event) {
+//           res.status(404).json(errorMessage.eventNotFound);
+//         } else {
+//           res.status(200).json(event);
+//         }
+//       })
+//       .catch((error) => {
+//         res.status(500).json(errorMessage.eventNotRetrieved);
+//       });
+// });
+
+// [GET] event by id with tasks
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+  db('events')
+      .where({id: id})
+      .first()
+      .then((event) => {
+        if (!event) {
+          res.status(404).json(errorMessage.eventNotFound);
+        } else {
+          db('tasks')
+              .where({event_id: id})
+              .select('id', 'task_name', 'task_completed')
+              .then((tasks => {
+                const tasksWithCompletedBooleans = tasks.map(task => ({
+                  ...task,
+                  task_completed: Boolean(task.task_completed)
+                }));
+                const eventWithActions = {
+                  ...event,
+                  tasks: tasksWithCompletedBooleans
+                };
+                res.status(200).json(eventWithActions);
+              }));
+        }
+      })
+      .catch((error) => {
+        res.status(500).json(errorMessage.eventNotRetrieved);
+      });
+});
