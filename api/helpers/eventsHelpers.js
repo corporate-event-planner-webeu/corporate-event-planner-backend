f53356405ea24fcfbcbb1dd6f24e22c5fe3ed7d7
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-25 12:40:08 +0100

    Add events helpers

---
+++ api/helpers/eventsHelpers.js
@@ -0,0 +1,32 @@
+const db = require('../../data/dbConfig');
+
+const getEvents = () => db('events');
+
+const getEventById = id => db('events')
+    .where({ id })
+    .first();
+
+const getEventsFromUser = id => db('events').where({ user_id: id });
+
+const addEvent = event => db('events')
+    .where({event_id: id })
+    .insert(event)
+    .then(ids => getEventById(ids[0]));
+
+const deleteEvent = id => db('events')
+    .where({ id })
+    .del();
+
+const updateEvent = (event, id) => db('events')
+    .where({ id })
+    .update(event);
+
+module.exports = {
+  getEvents,
+  getEventById,
+  getEventsFromUser,
+  addEvent,
+  deleteEvent,
+  updateEvent,
+};
+
